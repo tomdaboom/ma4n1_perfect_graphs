d5d86524e02d30ae682195dedc9b952abd1c3c01
Tom <tom.divers9@gmail.com>
2023-11-03 11:21:17 +0000

    imports

---
+++ perfect_graphs/computable_colourings.lean
@@ -0,0 +1,10 @@
+import Mathlib.Tactic
+import Mathlib.Combinatorics.SimpleGraph.Basic
+import Mathlib.Combinatorics.SimpleGraph.Coloring
+import Mathlib.Data.Fintype.Basic
+
+namespace ComputableColourings
+
+
+
+end ComputableColourings
