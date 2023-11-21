269c3912f9c3efe4ba66c81bca1ff54787c130ca
AlexChungCode <achungcode@gmail.com>
2023-11-21 14:09:56 +0000

    shiz

--- perfect_graphs/npartite-complete.lean
+++ perfect_graphs/npartite-complete.lean
@@ -7,23 +7,23 @@ import Mathlib.Data.Fintype.Basic
 
 
 
-theorem CompleteBipartiteGraph.chromaticNumber {V W : Type*} [Nonempty V] [Nonempty W] :
-    (completeBipartiteGraph V W).chromaticNumber = 2 := by
-  apply chromaticNumber_eq_card_of_forall_surj (CompleteBipartiteGraph.bicoloring V W)
-  intro C b
-  have v := Classical.arbitrary V
-  have w := Classical.arbitrary W
-  have h : (completeBipartiteGraph V W).Adj (Sum.inl v) (Sum.inr w) := by simp
-  have hn := C.valid h
-  by_cases he : C (Sum.inl v) = b
-  · exact ⟨_, he⟩
-  · by_cases he' : C (Sum.inr w) = b
-    · exact ⟨_, he'⟩
-    · exfalso
-      cases b <;>
-        simp only [Bool.eq_true_eq_not_eq_false, Bool.eq_false_eq_not_eq_true] at he he' <;>
-          rw [he, he'] at hn <;>
-        contradiction
+-- theorem CompleteBipartiteGraph.chromaticNumber {V W : Type*} [Nonempty V] [Nonempty W] :
+--     (completeBipartiteGraph V W).chromaticNumber = 2 := by
+--   apply chromaticNumber_eq_card_of_forall_surj (CompleteBipartiteGraph.bicoloring V W)
+--   intro C b
+--   have v := Classical.arbitrary V
+--   have w := Classical.arbitrary W
+--   have h : (completeBipartiteGraph V W).Adj (Sum.inl v) (Sum.inr w) := by simp
+--   have hn := C.valid h
+--   by_cases he : C (Sum.inl v) = b
+--   · exact ⟨_, he⟩
+--   · by_cases he' : C (Sum.inr w) = b
+--     · exact ⟨_, he'⟩
+--     · exfalso
+--       cases b <;>
+--         simp only [Bool.eq_true_eq_not_eq_false, Bool.eq_false_eq_not_eq_true] at he he' <;>
+--           rw [he, he'] at hn <;>
+--         contradiction
 
 
 
