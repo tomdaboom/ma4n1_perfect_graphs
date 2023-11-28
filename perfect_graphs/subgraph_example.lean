da6b42c8266b86d3b7468604d747f6d5881b0e2b
DanShomron <148561423+DanShomron@users.noreply.github.com>
2023-11-28 14:50:45 +0000

    Update subgraph_example.lean

--- perfect_graphs/subgraph_example.lean
+++ perfect_graphs/subgraph_example.lean
@@ -13,14 +13,6 @@ import Aesop.Tree.Data
 
 
 
-inductive color : Type
-| red : color
-| blue : color
-| green : color
-
--- open color
-
-
 
 namespace PerfectGraphs
 
@@ -39,16 +31,14 @@ def CliqueNumber {V : Type} (G : SimpleGraph V) (hasNclique): ℕ :=
 
 def G : SimpleGraph (Fin 5) where
   Adj x y  :=
-    --  notice that I removed the `if .. then .. else ..` since it was not necessary
     x = 0 ∧ y = 1 ∨ x = 1 ∧ y = 0 ∨ x = 2 ∧ y = 3 ∨ x = 3 ∧ y = 2
   symm a b h := by
-    --  `aesop` is a "search" tactic: among other things, it splits all cases and tries
-    --  various finishing tactics.
     aesop
   loopless a b := by
     aesop
     refine (?_ (id right.symm)).snd
     refine (?_ (id left.symm)).snd
+    done
     
 
 def G' : SimpleGraph (Fin 5) where
@@ -95,14 +85,34 @@ theorem ex : G'.IsSubgraph G := by
 def PGIsInduced {V : Type} (H : SimpleGraph V) (H' : Subgraph H) : Prop :=
   ∀ {v w : V}, v ∈ H'.verts → w ∈ H'.verts → H.Adj v w → H'.Adj v w
 
+def PGIsInduced' {V : Type} (H : SimpleGraph V) (H' : SimpleGraph V) : Prop :=
+  ∀ {v w : V}, (H.Adj v w → H'.Adj v w) ∨ (H'.neighborSet v = ∅) ∨ (H'.neighborSet w = ∅)
+
 theorem ex2 : PGIsInduced G G'' := by
  unfold G
  unfold G''
  unfold PGIsInduced
+ unfold Adj
  aesop
  exact Fin.rev_inj.mp (id left.symm)
  exact neg_add_eq_zero.mp (id right.symm)
- sorry
+ rw [← right] 
+ exact self_eq_add_left.mp right
+ exact self_eq_add_left.mp (id right.symm)
+ done
+
+theorem ex22 : PGIsInduced' G G' := by
+  unfold PGIsInduced'
+  unfold G
+  unfold G'
+  intros v w
+  cases v with
+  | mk val isLt => interval_cases val;
+                   · simp only; left; aesop; exact sub_eq_zero.mp (id left.symm); sorry
+                   · sorry
+                   · sorry
+                   · sorry
+                   · sorry
 
 def hasNClique {V : Type} (G : SimpleGraph V) (n : ℕ) : Prop :=
   ∃ t, G.IsNClique n t
@@ -110,15 +120,12 @@ def hasNClique {V : Type} (G : SimpleGraph V) (n : ℕ) : Prop :=
 noncomputable def CliqueNumber {V : Type} (G : SimpleGraph V) : ℕ :=
   sSup { n : ℕ | hasNClique G n }
 
--- theorem equivCliqueNumber {V : Type} (G : SimpleGraph V) (k : ℕ) (NClique : hasNClique G k) (notNPlusOneClique : ¬ hasNClique G (k+1)) : CliqueNumber G = k := by
---   sorry
+theorem equivCliqueNumber {V : Type} (G : SimpleGraph V) (k : ℕ) (NClique : hasNClique G k) (notNPlusOneClique : ¬ hasNClique G (k+1)) : CliqueNumber G = k := by
+  sorry
 
 /- Maybe can redefine this using cliqueSet -/
 
-theorem ex3 : IsNClique G 2 {0,1} := by
-  apply IsNClique.mk
-  unfold 
-  
+
 
 
 theorem faveExampleG : CliqueNumber G = 2 := by
@@ -126,15 +133,36 @@ theorem faveExampleG : CliqueNumber G = 2 := by
   apply equivCliqueNumber
   unfold hasNClique
   · use {0,1}
-    apply ex3
+    apply IsNClique.mk
+    unfold IsClique
+    unfold G
+    aesop_graph
+    norm_num
     
-  · intro f
-    norm_num at f
-    unfold hasNClique at f
-    refine Combinator.I ?notNPlusOneClique.a
-    aesop
+  · norm_num
+    unfold hasNClique
+    rw [@not_exists]
+    intro t
+    intro f
+    cases t with
+    | mk val nodup => interval_cases val
 
 
+def cycle (n : ℕ) : (SimpleGraph (ZMod n)) :=
+  SimpleGraph.fromRel (λ x y => x-y = 1)
+
+theorem CliqueNumberCycleIsTwo (n : ℕ) : CliqueNumber (cycle n) = 2 := by
+  unfold CliqueNumber
+  apply equivCliqueNumber
+  unfold hasNClique
+  · use {0,1}
+    apply IsNClique.mk
+    unfold IsClique
+    unfold cycle
+    aesop_graph
+    norm_num
+  · 
 
 
+    
     
