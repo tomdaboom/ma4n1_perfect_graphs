f864d04a91e48bd29ebbec3ee9633b3cb5efb58a
AlexChungCode <achungcode@gmail.com>
2023-11-28 11:58:48 +0000

    graphfromrel constructions

---
+++ perfect_graphs/GraphFromRel.lean
@@ -0,0 +1,7 @@
+import Mathlib.Tactic
+import Mathlib.Combinatorics.SimpleGraph.Basic
+--import Mathlib.Combinatorics.SimpleGraph.Basic
+--import Mathlib.Combinatorics.SimpleGraph.Clique
+import Mathlib.Combinatorics.SimpleGraph.Coloring
+--import Mathlib.Combinatorics.SimpleGraph.chromaticNumber
+import Mathlib.Data.Fintype.Basic
