c9ee89e9f57b68f0151a05f2cf43e2d0cdf548c4
Tom Divers <49053682+tomdaboom@users.noreply.github.com>
2024-01-26 17:14:47 +0000

    missing imports

--- perfect_graphs/dans_work.lean
+++ perfect_graphs/dans_work.lean
@@ -1,3 +1,132 @@
+--import Mathlib
+import Mathlib.Tactic
+import Mathlib.Combinatorics.SimpleGraph.Basic
+--import Mathlib.Combinatorics.SimpleGraph.Basic
+--import Mathlib.Combinatorics.SimpleGraph.Clique
+import Mathlib.Combinatorics.SimpleGraph.Coloring
+import Mathlib.Combinatorics.SimpleGraph.Subgraph
+import Mathlib.Combinatorics.SimpleGraph.Clique
+--import Mathlib.Combinatorics.SimpleGraph.chromaticNumber
+import Mathlib.Data.Fintype.Basic
+import Aesop.Check
+import Mathlib.Logic.Basic
+import Aesop.Tree.Data
+set_option trace.aesop true
+
+
+
+namespace PerfectGraphs
+
+#check SimpleGraph (Fin 5)
+
+
+
+
+
+
+def G : SimpleGraph (Fin 5) where
+  Adj x y  :=
+    x = 0 ∧ y = 1 ∨ x = 1 ∧ y = 0 ∨ x = 2 ∧ y = 3 ∨ x = 3 ∧ y = 2
+  symm a b h := by
+    aesop
+  loopless a b := by
+    aesop
+    revert right
+    sorry
+    sorry
+
+
+def G' : SimpleGraph (Fin 5) where
+  Adj x y  :=
+    --  notice that I removed the `if .. then .. else ..` since it was not necessary
+    x = 0 ∧ y = 1 ∨ x = 1 ∧ y = 0
+  symm a b h := by
+    --  `aesop` is a "search" tactic: among other things, it splits all cases and tries
+    --  various finishing tactics.
+    aesop
+  loopless a b := by
+    aesop
+
+open SimpleGraph
+def G'' : Subgraph G where
+ verts := {0, 1}
+ Adj x y := x = 0 ∧ y = 1 ∨ x = 1 ∧ y = 0
+ adj_sub := by
+  intros v w
+  intro f
+  unfold Adj G
+  aesop
+ edge_vert := by
+  aesop
+ symm Symmetric Adj := by aesop_graph
+
+
+
+open SimpleGraph
+theorem subg : G' ≤ G := by
+  unfold G;
+  unfold G';
+  aesop_graph
+
+#check G.Iso G'
+
+#check (G'' : Subgraph G)
+
+#check G'.IsSubgraph G
+
+theorem ex : G'.IsSubgraph G := by
+  unfold IsSubgraph
+  unfold G G'
+  aesop
+
+def PGIsInduced {V : Type} (H : SimpleGraph V) (H' : Subgraph H) : Prop :=
+  ∀ {v w : V}, v ∈ H'.verts → w ∈ H'.verts → H.Adj v w → H'.Adj v w
+
+def PGIsInduced' {V : Type} (H : SimpleGraph V) (H' : SimpleGraph V) : Prop :=
+  ∀ {v w : V}, (H.Adj v w → H'.Adj v w) ∨ (H'.neighborSet v = ∅) ∨ (H'.neighborSet w = ∅)
+
+def F := G.toSubgraph G' subg
+#check F
+
+open Subgraph
+
+theorem ex22 : F.IsInduced := by
+  unfold F
+  unfold IsInduced
+  unfold G
+  unfold G'
+  unfold toSubgraph
+  unfold SimpleGraph.Adj
+  simp
+  intros v w
+
+
+
+theorem ex2 : PGIsInduced G G'' := by
+ unfold G
+ unfold G''
+ unfold PGIsInduced
+ aesop
+ exact Fin.rev_inj.mp (id left.symm)
+ exact neg_add_eq_zero.mp (id right.symm)
+ rw [← right]
+ exact self_eq_add_left.mp right
+ exact self_eq_add_left.mp (id right.symm)
+ done
+
+theorem ex202 : PGIsInduced' G G' := by
+  unfold PGIsInduced'
+  unfold G
+  unfold G'
+  intros v w
+  cases v with
+  | mk val isLt => interval_cases val;
+                   · simp only; left; aesop; exact sub_eq_zero.mp (id left.symm); sorry
+                   · sorry
+                   · sorry
+                   · sorry
+                   · sorry
+
 def hasNClique {V : Type} (G : SimpleGraph V) (n : ℕ) : Prop :=
   ∃ t, G.IsNClique n t
 
